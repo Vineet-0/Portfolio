1e3193ffed63aff77eac462eabce0f223d408fe3
Vineet-0 <vineet351016@gmail.com>
2023-07-04 16:57:58 +0530

    nearly completed

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings:["Frontend Developer", "Web Developer"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    cursorChar:"_",
+    loop:true
+});
